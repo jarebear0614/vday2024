47597c2b9332be26b8116b5348130883c53da028
Arc1llusion <arc1llusion@gmail.com>
2025-01-30 08:19:11 -0500

    On Screen controls and some third party libraries

--- src/global.d.ts
+++ src/global.d.ts
@@ -69,3 +69,56 @@ declare module '*.fs' {
     const src: string
     export default src
 }
+
+declare module 'phaser-ui-tools' {    
+
+    export enum Alignment {
+        TOP_LEFT = 0,
+        TOP_CENTER = 1,
+        TOP_RIGHT = 2,
+        LEFT_TOP = 3,
+        LEFT_CENTER = 4,
+        LEFT_BOTTOM = 5,
+        CENTER = 6,
+        RIGHT_TOP = 7,
+        RIGHT_CENTER = 8,
+        RIGHT_BOTTOM = 9,
+        BOTTOM_LEFT = 10,
+        BOTTOM_CENTER = 11,
+        BOTTOM_RIGHT = 12,
+    }
+    
+
+    export class Group extends Phaser.GameObjects.Container {
+
+    }
+
+    export class Frame  extends Group{
+        constructor(game: Phaser.Scene, x?: number, y?: number, bg?: string, modal?: boolean);
+
+        dismiss() : void;
+
+        addNode(node: Object, paddingX?: number, paddingY?: number, alignment?: Alignment) : void;
+    }
+
+    export class Row extends Frame {
+        
+    }
+
+    export class Column extends Frame {
+
+    }
+
+    export class VectorPoint {
+        constructor(x?: number, y?:number, z?:number, sprite?: Object, position?: number);
+
+        x: number;
+        y: number;
+        z: number;
+
+    }
+
+    export class Wheel3D {
+        constructor(game: Phaser.Scene, xy: Object, sprites: any[], firstPlace: number, zoom: number, axis: string, rotations: Object, visibleRange: Object | null, tweenParams: Object)
+    }
+}
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,6 +1,10 @@
-import { Scene } from 'phaser';
+//import { Scene } from 'phaser';
 
-export class Game extends Scene
+import { Align, BaseScene } from 'phaser-utility';
+
+import { Row, VectorPoint, Wheel3D } from 'phaser-ui-tools';
+
+export class Game extends BaseScene
 {
     camera: Phaser.Cameras.Scene2D.Camera;
     background: Phaser.GameObjects.Image;
@@ -13,6 +17,16 @@ export class Game extends Scene
 
     playerVelocity: number = 250;
 
+    isUpDown: boolean = false;
+    isLeftDown: boolean = false;
+    isRightDown: boolean = false;
+    isDownDown: boolean = false;
+
+    isPreviousUpDown: boolean = false;
+    isPreviousLeftDown: boolean = false;
+    isPreviousRightDown: boolean = false;
+    isPreviousDownDown: boolean = false;
+
     constructor ()
     {
         super('Game');
@@ -20,7 +34,14 @@ export class Game extends Scene
 
     preload() 
     {
-        this.load.image('background-tiles', '/assets/map/background.png');
+        //controls
+        this.load.image('dpadup', '/assets/controls/dpad-up.png');
+        this.load.image('dpaddown', '/assets/controls/dpad-down.png');
+        this.load.image('dpadleft', '/assets/controls/dpad-left.png');
+        this.load.image('dpadright', '/assets/controls/dpad-right.png');
+
+        this.load.image('overworldTiles', '/assets/map/overworld-extruded.png');
+        this.load.image('interiorTiles', '/assets/map/interior-extruded.png');
         this.load.tilemapTiledJSON('map', '/assets/map/main.tmj')
 
         this.load.image('player', 'assets/player.png');
@@ -29,51 +50,157 @@ export class Game extends Scene
 
     create ()
     {
+        super.create();
+
+
+
         this.camera = this.cameras.main;
         this.camera.setBackgroundColor(0x00ff00);
         
         const map = this.make.tilemap({key: 'map'});
-        const tileset = map.addTilesetImage('background', 'background-tiles', 64, 64, 0, 0)!;
+        const tileset = map.addTilesetImage('overworld', 'overworldTiles', 16, 16, 1, 3)!;
+        const interior = map.addTilesetImage('interior', 'interiorTiles', 16, 16, 1, 3)!;
         map.createLayer('background', tileset, 0, 0);
+        map.createLayer('backgroundjuice', tileset, 0, 0);
+        map.createLayer('scenery', interior, 0, 0);
+        //map.createFromObjects
 
         this.xLimit = map.widthInPixels;
         this.yLimit = map.heightInPixels;
 
         this.player = this.physics.add.sprite(100, 100, "player");
         this.cursors = this.input.keyboard?.createCursorKeys();
+        
+        
+        this.makeGrid(11, 11);
+        this.getGrid().showNumbers();
 
-        this.input.once('pointerdown', () => {
+        this.getGrid().placeAt(2,2, this.player);
+        Align.scaleToGameW(this.player, .25, this);
 
-            this.scene.start('GameOver');
+        // this.input.once('pointerdown', () => {
 
+        //     this.scene.start('GameOver');
+
+        // });
+
+        let dpadTopLeft = {x: this.getW() * .10, y: this.getH() * .80};
+        let dpadPadding = 0;
+
+        let dpadup = this.add.image(0, 0, "dpadup").setInteractive();
+        dpadup.setScrollFactor(0);
+
+        let dpaddown = this.add.image(0, 0, "dpaddown").setInteractive();
+        dpaddown.setScrollFactor(0);
+        
+        let dpadleft = this.add.image(0, 0, "dpadleft").setInteractive();
+        dpadleft.setScrollFactor(0);
+
+        let dpadright = this.add.image(0, 0, "dpadright").setInteractive();
+        dpadright.setScrollFactor(0);
+
+        Align.scaleToGameW(dpadup, 0.06, this);
+        dpaddown.setScale(dpadup.scaleX, dpadup.scaleY);
+        dpadleft.setScale(dpadup.scaleX, dpadup.scaleY);
+        dpadright.setScale(dpadup.scaleX, dpadup.scaleY);
+
+        dpadup.setX(dpadTopLeft.x + dpadleft.displayWidth + dpadPadding);
+        dpadup.setY(dpadTopLeft.y);
+
+        dpaddown.setX(dpadTopLeft.x + dpadleft.displayWidth + dpadPadding);
+        dpaddown.setY(dpadTopLeft.y + dpadup.displayHeight + dpadup.displayHeight + dpadPadding);
+
+        dpadleft.setX(dpadTopLeft.x);
+        dpadleft.setY(dpadTopLeft.y + dpadup.displayHeight + dpadPadding);
+
+        dpadright.setX(dpadTopLeft.x + dpadleft.displayWidth + dpadleft.displayWidth + dpadPadding);
+        dpadright.setY(dpadTopLeft.y + dpadup.displayHeight + dpadPadding);
+
+        dpadup.on('pointerdown', () => {
+            console.log('down');
+            this.player.setVelocityY(-this.playerVelocity);
         });
+        dpadup.on('pointerup', () => {
+            console.log('up');
+            this.player.setVelocity(0, 0);
+        });
+
+        dpaddown.on('pointerdown', () => {
+            this.player.setVelocityY(this.playerVelocity);
+        });
+        dpaddown.on('pointerup', () => {
+            this.player.setVelocity(0, 0);
+        });
+
+        dpadleft.on('pointerdown', () => {
+            this.player.setVelocityX(-this.playerVelocity);
+        });
+        dpadleft.on('pointerup', () => {
+            this.player.setVelocity(0, 0);
+        });
+
+        dpadright.on('pointerdown', () => {
+            this.player.setVelocityX(this.playerVelocity);
+        });
+        dpadright.on('pointerup', () => {
+            this.player.setVelocity(0, 0);
+        });
+
+        //dpadup.addListener("click",)
+
+        
+
+
+
+        // this.getGrid().placeAtIndex(100, row, true);
+        
+        // //Align.scaleToGameW(row, .25, this);
+        // let gw = this.getW();
+        // let newDisplayWidth = gw * .60;
+
+        // let scale = newDisplayWidth / gw;
+
+        // row.setScale(scale, scale);
     }
 
     update() 
     {   
-        this.player.setVelocity(0, 0);
+        this.isPreviousUpDown = this.isUpDown;
+        this.isPreviousLeftDown = this.isLeftDown;
+        this.isPreviousRightDown = this.isRightDown;
+        this.isPreviousDownDown = this.isDownDown;
 
-        if (this.cursors?.up.isDown) 
+        this.isUpDown = this.cursors!.up.isDown;
+        this.isLeftDown = this.cursors!.left.isDown;
+        this.isRightDown = this.cursors!.right.isDown;
+        this.isDownDown = this.cursors!.down.isDown;
+
+        if (this.isUpDown) 
         {
             this.player.setVelocityY(-this.playerVelocity);
         }
-        if (this.cursors?.down.isDown) 
+        if (this.isDownDown) 
         {
             this.player.setVelocityY(this.playerVelocity);
         }
 
-        if (this.cursors?.right.isDown) 
+        if (this.isRightDown) 
         {
             this.player.setVelocityX(this.playerVelocity);
         }
-        if (this.cursors?.left.isDown) 
+        if (this.isLeftDown) 
         {
             this.player.setVelocityX(-this.playerVelocity);
         }
 
+        if( (!this.isUpDown && this.isPreviousUpDown) ||
+            (!this.isLeftDown && this.isPreviousLeftDown) ||
+            (!this.isRightDown && this.isPreviousRightDown) ||
+            (!this.isDownDown && this.isPreviousDownDown)) {
+                this.player.setVelocity(0, 0);
+        }
+
         this.cameras.main.centerOn(this.player.x, this.player.y);      
-        console.log(this.player.x);
-        console.log(this.xLimit)  	;
         this.cameras.main.setBounds(0, 0, this.xLimit, this.yLimit);
     }
 }
