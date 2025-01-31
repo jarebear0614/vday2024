aafaaa2329f4061c9bc25dc65d9eab7b3732a554
Arc1llusion <arc1llusion@gmail.com>
2025-01-31 15:29:25 -0500

    Control and map work

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -16,7 +16,12 @@ export class Game extends BaseScene
     yLimit: number = 0;
     tilemapScale: number = 0;
 
-    playerVelocity: number = 250;
+    playerVelocity: number = 256;
+
+    isTouchUpDown: boolean = false;
+    isTouchLeftDown: boolean = false;
+    isTouchRightDown: boolean = false;
+    isTouchDownDown: boolean = false;
 
     isUpDown: boolean = false;
     isLeftDown: boolean = false;
@@ -42,12 +47,6 @@ export class Game extends BaseScene
 
     preload() 
     {
-        //controls
-        this.load.image('dpadup', 'assets/controls/dpad-up.png');
-        this.load.image('dpaddown', 'assets/controls/dpad-down.png');
-        this.load.image('dpadleft', 'assets/controls/dpad-left.png');
-        this.load.image('dpadright', 'assets/controls/dpad-right.png');
-
         this.load.image('dpadfull', 'assets/controls/dpadfull.png');
 
         this.load.image('overworldTiles', 'assets/map/overworld-extruded.png');
@@ -69,14 +68,15 @@ export class Game extends BaseScene
         
         this.configureTilemaps();
 
-        let cols = this.getW() / (TILE_SIZE * this.tilemapScale);
-        let rows = this.getH() / (TILE_SIZE * this.tilemapScale);
+        // let cols = this.getW() / (TILE_SIZE * this.tilemapScale);
+        // let rows = this.getH() / (TILE_SIZE * this.tilemapScale);
 
-        this.makeGrid(rows, cols);  
-        this.configureInput();        
+        // this.makeGrid(rows, cols);  
+          
         this.configurePlayer();
+        this.configureInput();      
         
-        this.getGrid().placeAt(10,10, this.player);
+        this.player.setPosition(640, 640);
     }
 
     update() 
@@ -86,10 +86,10 @@ export class Game extends BaseScene
         this.isPreviousRightDown = this.isRightDown;
         this.isPreviousDownDown = this.isDownDown;
 
-        this.isUpDown = this.cursors!.up.isDown;
-        this.isLeftDown = this.cursors!.left.isDown;
-        this.isRightDown = this.cursors!.right.isDown;
-        this.isDownDown = this.cursors!.down.isDown;
+        this.isUpDown = this.isTouchUpDown || this.cursors!.up.isDown;
+        this.isLeftDown = this.isTouchLeftDown || this.cursors!.left.isDown;
+        this.isRightDown = this.isTouchRightDown || this.cursors!.right.isDown;
+        this.isDownDown = this.isTouchDownDown || this.cursors!.down.isDown;
 
         let vel: Phaser.Math.Vector2 = new Phaser.Math.Vector2();
 
@@ -113,7 +113,9 @@ export class Game extends BaseScene
 
         vel = vel.normalize().scale(this.playerVelocity);
 
+
         if(vel.lengthSq() > 0) {
+            console.log(vel);
             this.player.setVelocity(vel.x, vel.y);        
         }
 
@@ -131,21 +133,19 @@ export class Game extends BaseScene
     configurePlayer() {
         this.player = this.physics.add.sprite(100, 100, "characters", 0);
 
-        let c = new Character(this, 200, 200).create();
+        let c = new Character(this, 400, 400).create();
         
         this.cursors = this.input.keyboard?.createCursorKeys();
-        Align.scaleToGameW(this.player, TILE_SCALE, this);        
-
+        Align.scaleToGameW(this.player, TILE_SCALE, this); 
         
         this.backgroundJuiceLayer = this.map.createLayer('above', this.overworldTileset, 0, 0)!;
         this.backgroundCollidersLayer = this.map.createLayer('colliders', this.overworldTileset, 0, 0)!;
-
         
         this.backgroundJuiceLayer?.setScale(this.tilemapScale, this.tilemapScale);
         this.backgroundCollidersLayer.setScale(this.tilemapScale, this.tilemapScale);
 
         this.physics.add.collider(this.player, this.backgroundCollidersLayer);
-        this.backgroundCollidersLayer.setCollisionBetween(2, 646, true, false);
+        this.backgroundCollidersLayer.setCollisionByExclusion([-1], true);
     }
 
     configureTilemaps() {        
@@ -153,7 +153,7 @@ export class Game extends BaseScene
         this.overworldTileset = this.map.addTilesetImage('overworld', 'overworldTiles', 16, 16, 1, 3)!;
         const interior = this.map.addTilesetImage('interior', 'interiorTiles', 16, 16, 1, 3)!;
         let backgroundLayer = this.map.createLayer('ground', this.overworldTileset, 0, 0);
-        let sceneryLayer = this.map.createLayer('scenery', interior, 0, 0);        
+        let sceneryLayer = this.map.createLayer('ground_decoration', this.overworldTileset, 0, 0);        
 
         this.tilemapScale = (this.getW() * TILE_SCALE) / TILE_SIZE;
         backgroundLayer?.setScale(this.tilemapScale, this.tilemapScale);
@@ -166,7 +166,7 @@ export class Game extends BaseScene
 
     configureInput() {
 
-        let dpadTopLeft = {x: this.getW() * .20, y: this.getH() * .80};
+        let dpadTopLeft = {x: this.getW() * .85, y: this.getH() * .05};
         let dpadfull = this.add.image(0, 0, 'dpadfull').setScrollFactor(0);        
 
         Align.scaleToGameW(dpadfull, 0.18, this);
@@ -197,54 +197,54 @@ export class Game extends BaseScene
         // this.add.rectangle(dpadTopLeft.x - dpadWidth / 2 - 5, dpadTopLeft.y + dpadWidth + 5, dpadWidth - 10, dpadWidth - 10, 0x000000).setScrollFactor(0);
         // this.add.rectangle(dpadTopLeft.x + dpadWidth / 2 + 5, dpadTopLeft.y + dpadWidth + 5, dpadWidth - 10, dpadWidth - 10, 0x333333).setScrollFactor(0);
         
-        this.input.on('gameobjectdown', (pointer: Object, gameObject: Phaser.GameObjects.GameObject) => {
+        this.input.on('gameobjectover', (pointer: Object, gameObject: Phaser.GameObjects.GameObject) => {
+
             let name = gameObject.name;
-            let vel: Phaser.Math.Vector2 = new Phaser.Math.Vector2();
 
             switch(name) {
                 case 'controlsUp':
-                    vel.y = -this.playerVelocity;
+                    this.isTouchUpDown = true;
                     break;
                 case 'controlsDown':
-                    vel.y = this.playerVelocity;
+                    this.isTouchDownDown = true;
                     break;
                 case 'controlsLeft':
-                    vel.x = -this.playerVelocity;
+                    this.isTouchLeftDown = true;
                     break;
                 case 'controlsRight':
-                    vel.x = this.playerVelocity;
+                    this.isTouchRightDown = true;
                     break;
                 case 'controlsUpLeft':
-                    vel.x = -this.playerVelocity;
-                    vel.y = -this.playerVelocity;
+                    this.isTouchUpDown = true;
+                    this.isTouchLeftDown = true;
                     break;
                 case 'controlsUpRight':
-                    vel.x = this.playerVelocity;
-                    vel.y = -this.playerVelocity;
+                    this.isTouchUpDown = true;
+                    this.isTouchRightDown = true;
                     break;
                 case 'controlsDownLeft':
-                    vel.x = -this.playerVelocity;
-                    vel.y = this.playerVelocity;
+                    this.isTouchDownDown = true;
+                    this.isTouchLeftDown = true;
                     break;
                 case 'controlsDownRight':
-                    vel.x = this.playerVelocity;
-                    vel.y = this.playerVelocity;
+                    this.isTouchDownDown = true;
+                    this.isTouchRightDown = true;
                     break;
-            }
-
-            vel = vel.normalize().scale(this.playerVelocity);
-
-            if(vel.lengthSq() > 0) {
-                this.player.setVelocity(vel.x, vel.y);        
             }
         });
 
         this.input.on('gameobjectup', (pointer: Object, gameObject: Phaser.GameObjects.GameObject) => {
-            this.player.setVelocity(0,0);
+            this.isTouchUpDown = false;
+            this.isTouchLeftDown = false;
+            this.isTouchRightDown = false;
+            this.isTouchDownDown = false;
         });
 
         this.input.on('gameobjectout', (pointer: Object, gameObject: Phaser.GameObjects.GameObject) => {
-            this.player.setVelocity(0,0);
+            this.isTouchUpDown = false;
+            this.isTouchLeftDown = false;
+            this.isTouchRightDown = false;
+            this.isTouchDownDown = false;
         });
     }
 }
